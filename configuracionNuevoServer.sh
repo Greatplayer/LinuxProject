320703b8490c1fb72c7d23620f44786a68309fa3
Greatplayer <greatplayer125@gmail.com>
2019-07-17 15:27:41 -0400

    Update configuracionNuevoServer.sh
    correciones

--- configuracionNuevoServer.sh
+++ configuracionNuevoServer.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 ipCompartir=$1
+carpeta=/migracionBackupServer
+opciones="(rw,sync,no_root_squash,no_all_squash)"
 
 read -p "IP ingresado: $1"
 
 read -p "Creando Archivos de test para demostrar la migracion"
 touch /home/ejem1.txt
 touch /home/ejem2.txt
-pause
+
 
 read -p "Creando carpeta para hospedar el bera-backup"
 mkdir /HerramientaBera
 read -p "Descargandolo del repositorio"
 git clone https://github.com/oslcixug/bera-backup.git --depth 1 --branch=master /HerramientaBera
-pause
+
 
 read -p "Descargando software de NFS Utils"
 yum install nfs-utils
@@ -32,7 +34,8 @@ systemctl start nfs-lock
 systemctl start nfs-idmap
 
 read -p "Agregando la IP origen del backup"
-echo /migracionBackupServer $ipCompartir (rw,sync,no_root_squash,no_all_squash)  >> /etc/exports
+
+echo $carpeta $ipCompartir $opciones  >> /etc/exports
 
 read -p "Reiniciando el servicio de compartido"
 exportsfs -a
