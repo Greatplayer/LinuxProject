e880db7e52fdcf12c11a4830b18f3d25d9db1b34
Greatplayer <greatplayer125@gmail.com>
2019-07-17 16:14:03 -0400

    Update configuracionNuevoServer.sh

--- configuracionNuevoServer.sh
+++ configuracionNuevoServer.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 set -x
-
 ipCompartir=$1
 carpeta=/migracionBackupServer
 opciones="(rw,sync,no_root_squash,no_all_squash)"
 
-read -p "IP ingresado: $1"
+read -p "----------------------------IP ingresado: $1--------------"
 
-read -p "Creando Archivos de test para demostrar la migracion"
+read -p "----------------------------Creando Archivos de test para demostrar la migracion--------------"
 touch /home/ejem1.txt
 touch /home/ejem2.txt
 
 
-read -p "Creando carpeta para hospedar el bera-backup"
+read -p "----------------------------Creando carpeta para hospedar el bera-backup--------------"
 mkdir /HerramientaBera
-read -p "Descargandolo del repositorio"
+read -p "----------------------------Descargandolo del repositorio--------------"
 git clone https://github.com/oslcixug/bera-backup.git --depth 1 --branch=master /HerramientaBera
 
 
-read -p "Descargando software de NFS Utils"
+read -p "--------------Descargando software de NFS Utils--------------"
 yum install nfs-utils
-read -p "Creación de carpeta compartida con permisos"
+read -p "--------------Creación de carpeta compartida con permisos--------------"
 mkdir /migracionBackupServer
 chmod -R 755 /migracionBackupServer
 chown nfsnobody:nfsnobody /migracionBackupServer
 
-read -p "Habilitando los servicios"
+read -p "--------------Habilitando los servicios--------------"
 systemctl enable nfs-server
 systemctl enable nfs-lock
 systemctl enable nfs-idmap
@@ -34,19 +33,19 @@ systemctl start nfs-server
 systemctl start nfs-lock
 systemctl start nfs-idmap
 
-read -p "Agregando la IP origen del backup"
+read -p "--------------Agregando la IP origen del backup--------------"
 
 echo $carpeta $ipCompartir$opciones>> /etc/exports
 
-read -p "Reiniciando el servicio de compartido"
-exportfs -a
+read -p "--------------Reiniciando el servicio de compartido--------------"
+exportsfs -a
 systemctl restart nfs-server
 
-read -p "configurando el firewall"
+read -p "--------------configurando el firewall--------------"
 firewall-cmd --permanent --zone=public --add-service=nfs
 firewall-cmd --permanent --zone=public --add-service=mountd
 firewall-cmd --permanent --zone=public --add-service=rpc-bind
 firewall-cmd --reload
 
-read -p "Listar los compartidos"
+read -p "--------------Listar los compartidos--------------"
 exportfs
