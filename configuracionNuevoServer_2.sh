bbe0cf0503b3600919adb2315e3b6589a4206110
Greatplayer <greatplayer125@gmail.com>
2019-07-18 16:53:31 -0400

    Update configuracionNuevoServer_2.sh

--- configuracionNuevoServer_2.sh
+++ configuracionNuevoServer_2.sh
@@ -12,6 +12,6 @@ read -p "--------------Desempaquetar el archivo tar--------------"
 tar -xvf /backupRestaura/backup190720.tar
 
 read -p "--------------Copiar la configuracion de restauracion--------------"
-cp /HerramientaBera/_bera_restore_config_example _bera_config
+cp /HerramientaBera/_bera_restore_config_example /HerramientaBera/_bera_config
 
 echo "Final de script: configuracionNuevoServer_2.sh"
