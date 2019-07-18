ec36b0828debca068d71757172bee72ffff3f79d
Greatplayer <greatplayer125@gmail.com>
2019-07-18 15:53:07 -0400

    Update configuracionNuevoServer_2.sh

--- configuracionNuevoServer_2.sh
+++ configuracionNuevoServer_2.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+read -p "--------------Creando carpeta para descomprimir el archivo--------------"
+mkdir /backupRestaura
+chmod 777 /backupRestaura
+
+read -p "--------------Copiando archivo de backup generado en origen--------------"
+cp /migracionBackupServer/backup190720.tar /backupRestaura
+chmod 777 /migracionBackupServer/backup190720.tar
+
+read -p "--------------Desempaquetar el archivo tar--------------"
+tar -xvf /migracionBackupServer/backup190720.tar
+
+read -p "--------------Copiar la configuracion de restauracion--------------"
+cp /HerramientasBera/_bera_restore_config_example _bera_config
