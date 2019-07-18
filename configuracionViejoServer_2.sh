fdc04580e76d15b37054a230fdfbfd4c51fa970d
Greatplayer <greatplayer125@gmail.com>
2019-07-18 13:46:11 -0400

    Update configuracionViejoServer_2.sh
    Inicial

--- configuracionViejoServer_2.sh
+++ configuracionViejoServer_2.sh
@@ -1,1 +1,9 @@
 
+#!/bin/bash
+
+read -p "----------------------------Empaquetar el backup para la transmision--------------"
+tar cvf backup190720.tar /home/backups/backups
+chmod 777 /home/backups/backup190720.tar
+
+read -p "----------------------------Copiando el backup al nuevo servidor--------------"
+cp /home/backups/backup190720.tar /migracionServer
